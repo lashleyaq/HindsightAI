c818c117cb8de6b648c282619c6ee4e6e91f58dd
Kevin Howlett <54913419+Kevin-Howlett@users.noreply.github.com>
2021-12-01 18:17:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,12 +48,8 @@ def main():
     valid_files = videoCompatabilityCheck(os.listdir(FOLDER_PATH))
     valid_files.insert(0, "Select a file...")
     selected_filename = st.sidebar.selectbox("Select a video to search:", valid_files)    
-    segment_options = {'Low':4,'Medium':9,'High':16}
-    granularity = st.sidebar.selectbox(
-        "Select the granularity of the search:",
-        list(segment_options.keys())
-    )
-    num_segments = segment_options[granularity]
+
+    num_segments = 9
 
     # Reactivity
     if selected_filename != "Select a file...":
@@ -99,7 +95,7 @@ def main():
                 with search_phrase:
                     st.write(f"Searching the video for {keyword_search}...")
 
-                unfiltered = clipAnalyze(tensorDict, text, num_segments=num_segments)
+                unfiltered = clipDict(tensorDict, text, num_segments=num_segments)
 
                 try:
                     clipResult = filterResult(unfiltered, prediction_confidence / 100)
@@ -234,8 +230,39 @@ def videoToTensor(videoPath: str, framerate: int, selected_fps: int = 1):
     return tensorDict
 
 
-def clipAnalyze(tensorDict, searchString: str, num_segments: int):
-    """
+def clipAnalyze(tensor, searchString:str):
+    '''
+    Arguments:
+    ==========
+        tensor: tensor
+            A tensor from a single frame in the video 
+            being analyzed.
+
+        searchString: str
+            A user supplied string to tokenize and encode
+            for use as a clip imbedding for analysis.
+
+
+    Returns:
+    ========
+        result: float
+            A float representing the confidence calculated 
+            by clip, given the user's text query.
+    '''
+    
+    tokenizedText = clip.tokenize(searchString).to(DEVICE)  
+    
+    with torch.no_grad():
+        image = PREPROCESS(Image.fromarray(tensor)).unsqueeze(0).to(DEVICE)
+        logits_per_image, __ = MODEL(image, tokenizedText)
+        prob = logits_per_image.softmax(dim=-1).cpu().numpy()
+        result = np.round(prob[0][0],2)
+        
+    return result
+
+
+def clipDict(tensorDict, searchString:str, num_segments:int, confidenceLevel:float = 0.90):
+    '''
 
     Runs a clip analysis on each frame of the provided
     dictionary's values where the posisble text classes 
@@ -254,15 +281,19 @@ def clipAnalyze(tensorDict, searchString: str, num_segments: int):
             A user supplied string to tokenize and encode
             for use as a clip imbedding for analysis.
 
+        num_segments: str
+            Number of segments by which to split each 
+            of the frames for more detailed analysis.
+
     Returns:
     ========
-        sorted_result: dict
+        result: dict
             a dictonary of frame indicies and their corresponding
-            clip probabilities in descending order of probability e.g.:
+            clip probabilities e.g.:
                 sorted_result = {
                     'timeInSeconds' = float32(probability)
                 }
-    """
+    '''
 
     result = {}
 
@@ -270,47 +301,63 @@ def clipAnalyze(tensorDict, searchString: str, num_segments: int):
     progress_bar = st.progress(0)
     progress_bar_value = 0
     # scale progressbar increment value to the len(video)
-    incrementValue = 100 / len(list(tensorDict.items())) / 100
+    incrementValue = (100/len(list(tensorDict.items()))/100) 
 
-    tokenizedText = clip.tokenize(searchString).to(DEVICE)
 
-    with torch.no_grad():
-        for frameTensorPair in tensorDict.items():
-            progress_bar.progress(progress_bar_value)
-            im = frameTensorPair[1]
-            segments = []
-            indices = []
+    for frameTensorPair in tensorDict.items():
+        progress_bar.progress(progress_bar_value)
+        
+        # Analyze entire image with CLIP
+        im = frameTensorPair[1]
+        prob = clipAnalyze(im, searchString)
+        result[str(frameTensorPair[0])] = [prob, [0, im.shape[0], 0, im.shape[1]]]
 
-            for i in segment_np_array(im, num_segments):
-                segments.append(im[i[0] : i[1], i[2] : i[3], :])
-                indices.append([i[0], i[1], i[2], i[3]])
+            
+        # If image is a match to text, analyze segments of image
+        if result[str(frameTensorPair[0])][0] >= confidenceLevel:
+            highest_conf = result[str(frameTensorPair[0])][0]
 
-            probs = []
+            x1, x2, y1, y2 = None, None, None, None
 
-            for segment in segments:
-                image = PREPROCESS(Image.fromarray(segment)).unsqueeze(0).to(DEVICE)
-                logits_per_image, __ = MODEL(image, tokenizedText)
-                prob = logits_per_image.softmax(dim=-1).cpu().numpy()
-                probs.append(np.round(prob[0][0], 2))
+            while True:
 
-            prob = max(probs)
-            max_index = probs.index(prob)
-            index = indices[max_index]
+                segments = []
+                indices = []
+                for i in segment_np_array(im, num_segments, y1, y2, x1, x2):
+                    segments.append(im[i[0]:i[1],i[2]:i[3],:])
+                    indices.append([i[0],i[1],i[2],i[3]])
 
-            result[str(frameTensorPair[0])] = [prob, index]
+                probs = []
 
+                for segment in segments:
+                    probs.append(clipAnalyze(segment, searchString))
+
+                prob = max(probs)
+                max_index = probs.index(prob)
+                index = indices[max_index]
+
+                if prob < highest_conf - 0.05:
+            	    break
+
+                result[str(frameTensorPair[0])] = [prob, index]
+
+                y1 = index[0]
+                y2 = index[1]
+                x1 = index[2]
+                x2 = index[3]
+            
             progress_bar_value += incrementValue
-
+    
     progress_bar.empty()
 
     return result
 
 
-def filterResult(result, confidenceLevel: float = 0.90):
-    """
+def filterResult(result, confidenceLevel:float = 0.90):
+    '''
     Arguments:
     ==========
-        sortedResult: dict
+        result: dict
             A dictionary of frame-probability pairs e.g.:
                 sorted_result = {
                     'timeInseconds' = np.float(probability)
@@ -328,26 +375,25 @@ def filterResult(result, confidenceLevel: float = 0.90):
                     'timeInseconds' = np.float(probability)
                 }
     
-    """
-    sorted_result = dict(sorted(result.items(), key=lambda x: x[1][0], reverse=True))
+    '''
+    sorted_result = dict(sorted(result.items(), key=lambda x: x[1][0],reverse=True))
 
     filteredResult = {}
 
     for frameTensorPair in sorted_result.items():
         if frameTensorPair[1][0] > confidenceLevel:
             filteredResult[frameTensorPair[0]] = frameTensorPair[1]
-
-        elif len(filteredResult) == 0:
-            return (
-                filteredResult["No results with the selected confidence level"] == None
-            )
-
+        
+        elif len(filteredResult) == 0: 
+            return filteredResult['No results with the selected confidence level'] == None
+        
         else:
             return filteredResult
 
 
-def segment_np_array(arr, num_segments):
-    """
+def segment_np_array(arr, num_segments, y1=None, y2=None, x1=None, x2=None):
+    
+    '''
     Takes in a numpy array for a 3-channel image and a desired number
     of segments for the image. 
     
@@ -356,33 +402,39 @@ def segment_np_array(arr, num_segments):
     
     *** Must take in only perfect squares as the "num_segments"!***
     
-    """
-
+    '''
+    
+    if x1 == None:
+        x1 = 0
+        y1 = 0
+        x2 = arr.shape[1]
+        y2 = arr.shape[0]
+        
     box_dim = int(np.sqrt(num_segments))
-
-    image_h = arr.shape[0]
-    image_w = arr.shape[1]
-    kernel_size = int(np.floor(image_w / (box_dim * 0.83)))
-
+    
+    image_h = y2 - y1
+    image_w = x2 - x1
+    kernel_size = int(np.floor(image_w/(box_dim*0.80)))
+    
     if kernel_size > image_h:
         kernel_size = image_h
-
-    stride_w = int(np.floor((image_w - kernel_size) / (box_dim - 1)))
-    stride_h = int(np.floor((image_h - kernel_size) / (box_dim - 1)))
-
+    
+    stride_w = int(np.floor((image_w - kernel_size)/(box_dim-1)))
+    stride_h = int(np.floor((image_h - kernel_size)/(box_dim-1)))
+    
     segments = []
-    top = 0
-
+    top = y1
+    
     for i in range(box_dim):
         bottom = top + kernel_size
-        left = 0
+        left = x1
         for j in range(box_dim):
             right = left + kernel_size
-            segments.append((top, bottom, left, right))
+            segments.append((top,bottom,left,right))
             left += stride_w
-
+            
         top += stride_h
-
+            
     return segments
 
 
